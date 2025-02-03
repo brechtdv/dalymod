ba3bfdd5376ed9deaf1d63e2b4ea67c05a30b121
Brecht <brechtdv@gmail.com>
2025-02-03 19:48:43 +0100

    extend age range

--- R/dalymod-helpers.R
+++ R/dalymod-helpers.R
@@ -128,8 +128,8 @@ import_node <-
     dismod_node$age <- list()
     dismod_node$age$type <- "AGE"
     dismod_node$age$dist <- "age"  
-    dismod_node$age$data <- xl_node[(row_age+2):(row_sex-2), 2:15]  
-    colnames(dismod_node$age$data) <- xl_node[row_age+1, 2:15]
+    dismod_node$age$data <- xl_node[(row_age+2):(row_sex-2), 2:21]  
+    colnames(dismod_node$age$data) <- xl_node[row_age+1, 2:21]
     rownames(dismod_node$age$data) <- NULL
     
     ## setup node sex
